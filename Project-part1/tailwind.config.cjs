0a8991efae600ae96a75c1f903085b0dfc4334a5
yosef2000138 <80209511+yosef2000138@users.noreply.github.com>
2024-12-15 14:04:36 +0200

    initial

---
+++ Project-part1/tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.{html,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio")
+  ],
+};
